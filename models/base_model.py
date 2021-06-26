d9320a6ed10b9e1a688ce614f752991f36bedfa0
AlexisCORAN <alexis_ca_@outlook.com>
2021-06-26 01:42:56 +0000

    Added to_didct(self) method

--- models/base_model.py
+++ models/base_model.py
@@ -7,13 +7,18 @@ class BaseModel():
     
     def __init__(self):
         self.id = str(uuid4())
-        self.create_at = datetime.now()
-        self.updated_at = self.create_at
+        self.created_at = datetime.now()
+        self.updated_at = self.created_at
         
     def __str__(self):
         return ("[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__))
         
     def save(self):
         self.updated_at = datetime.now()
-
-
+    
+    def to_dict(self):
+        new_dict = self.__dict__.copy()
+        new_dict['__class__'] = self.__class__.__name__
+        new_dict['created_at'] = self.created_at.isoformat()
+        new_dict['updated_at'] = self.updated_at.isoformat()
+        return (new_dict)
