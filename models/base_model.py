ade0069634c160c8f28a8a935a62d14443ee1e09
AlexisCORAN <alexis_ca_@outlook.com>
2021-06-26 22:13:36 +0000

    Added documentacion

--- models/base_model.py
+++ models/base_model.py
@@ -1,22 +1,47 @@
 #!/usr/bin/env python3
-
+""""
+This module defines BaseModel class
+"""
 from uuid import uuid4
 from datetime import datetime
 
+
 class BaseModel():
-    
-    def __init__(self):
-        self.id = str(uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = self.created_at
-        
+    """Defines all common attributes/methods for other classes"""
+
+    def __init__(self, *args, **kwargs):
+        time_f = "%Y-%m-%dT%H:%M:%S.%f"
+
+        if len(kwargs) is not 0:
+                for key, value in kwargs.items():
+                    if key == "__class__":
+                        continue
+                    if key == "created_at" or key == "updated_at":
+                        self.__dict__[key] = datetime.strptime(value, time_f)
+                    else:
+                        self.__dict__[key] = value
+        else:
+            self.id = str(uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
+
     def __str__(self):
-        return ("[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__))
-        
+        return ("[{}] ({}) {}".format(self.__class__.__name__,
+                                      self.id,
+                                      self.__dict__))
+
     def save(self):
+        """
+        Updates the public instance attribute
+        updated_at with the current datetime
+        """
         self.updated_at = datetime.now()
-    
+
     def to_dict(self):
+        """
+        Returns a dictionary containing all keys/values
+        of __dict__ of the instance
+        """
         new_dict = self.__dict__.copy()
         new_dict['__class__'] = self.__class__.__name__
         new_dict['created_at'] = self.created_at.isoformat()
