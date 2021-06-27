e2987b23a2f1167575f149e6f9f5504c7c1772a5
renatoleon <2829@holbertonschool.com>
2021-06-27 00:39:21 +0000

    Make State to inherit from BaseModel

--- models/state.py
+++ models/state.py
@@ -2,8 +2,9 @@
 """"
 This module defines State class
 """
+from models.base_model import BaseModel
 
 
-class State():
+class State(BaseModel):
     """ Defines all common attributes/methods State """
     name = ''
