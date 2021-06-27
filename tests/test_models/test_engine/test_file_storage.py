028846d5e40da785baae7c087591361b0abae27d
renatoleon <2829@holbertonschool.com>
2021-06-27 00:14:42 +0000

    Create test file for FileStorage

---
+++ tests/test_models/test_engine/test_file_storage.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+    This module contains test cases for FileStorage
+"""
+import unittest
+from models.engine.file_storage import FileStorage
+
+
+class TestFileStorage(unittest.TestCase):
+    """" Test cases class """
+    pass
