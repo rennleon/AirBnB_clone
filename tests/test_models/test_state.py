51f5d90d8caff79087ed7e2aaa119a2dcd12db07
renatoleon <2829@holbertonschool.com>
2021-06-27 00:40:50 +0000

    Create test file for State

---
+++ tests/test_models/test_state.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+    This module contains test cases for State
+"""
+import unittest
+from models.state import State
+
+
+class TestState(unittest.TestCase):
+    """" Test cases class """
+    pass
