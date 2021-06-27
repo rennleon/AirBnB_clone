f153ff13626c80124606f60a9f13f45cba2a951a
renatoleon <2829@holbertonschool.com>
2021-06-27 00:55:32 +0000

    Create test file for Place class

---
+++ tests/test_models/test_place.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+    This module contains test cases for Place
+"""
+import unittest
+from models.place import Place
+
+
+class TestPlace(unittest.TestCase):
+    """" Test cases class """
+    pass
