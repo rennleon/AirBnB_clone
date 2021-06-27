a3e41594b2f2ee865034b8b064dedba586ee8f6b
renatoleon <2829@holbertonschool.com>
2021-06-27 00:10:19 +0000

    Update base_model.py to link BaseModel to FileStorage by using the variable storage

--- models/base_model.py
+++ models/base_model.py
@@ -2,6 +2,7 @@
 """"
 This module defines BaseModel class
 """
+import models
 from uuid import uuid4
 from datetime import datetime
 
@@ -13,22 +14,27 @@ class BaseModel():
         time_f = "%Y-%m-%dT%H:%M:%S.%f"
 
         if len(kwargs) is not 0:
-                for key, value in kwargs.items():
-                    if key == "__class__":
-                        continue
-                    if key == "created_at" or key == "updated_at":
-                        self.__dict__[key] = datetime.strptime(value, time_f)
-                    else:
-                        self.__dict__[key] = value
+            for key, value in kwargs.items():
+                if key == "__class__":
+                    continue
+                if key == "created_at" or key == "updated_at":
+                    self.__dict__[key] = datetime.strptime(value, time_f)
+                else:
+                    self.__dict__[key] = value
         else:
             self.id = str(uuid4())
             self.created_at = datetime.now()
             self.updated_at = self.created_at
+            models.storage.new(self)
 
     def __str__(self):
-        return ("[{}] ({}) {}".format(self.__class__.__name__,
-                                      self.id,
-                                      self.__dict__))
+        return (
+                "[{}] ({}) {}"
+                .format(self.__class__.__name__,
+                        self.id,
+                        self.__dict__
+                        )
+                )
 
     def save(self):
         """
@@ -36,6 +42,7 @@ class BaseModel():
         updated_at with the current datetime
         """
         self.updated_at = datetime.now()
+        models.storage.save()
 
     def to_dict(self):
         """
