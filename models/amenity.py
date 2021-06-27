2f5af802f4705f0fdfbaf051e7c1cfb9d5163150
renatoleon <2829@holbertonschool.com>
2021-06-27 00:46:22 +0000

    Create Amenity class

---
+++ models/amenity.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""""
+This module defines Amenity class
+"""
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """ Defines all common attributes/methods for Amenity """
+    name = ''
