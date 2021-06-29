d1bc382bfb580cbb2dc9a4f6dd2639e738ca3b06
renatoleon <2829@holbertonschool.com>
2021-06-29 13:01:38 +0000

    Update testfile

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -1,11 +1,20 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 """
     This module contains test cases for FileStorage
 """
 import unittest
+from models import engine
 from models.engine.file_storage import FileStorage
 
 
 class TestFileStorage(unittest.TestCase):
     """" Test cases class """
-    pass
+    def setUp(self):
+        super().setUp()
+        self.storage = FileStorage()
+
+    def test_method_all(self):
+        empty_dict = dict()
+        all = self.storage.all()
+
+        self.assertDictEqual(empty_dict, all)
