942f32fff3760b0effc4ff4cc4382cdbdbf8a1c7
renatoleon <2829@holbertonschool.com>
2021-06-29 18:48:22 +0000

    Add condition to verify that file path exists when attempting to open

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -127,11 +127,12 @@ class TestFileStorage(unittest.TestCase):
 
         self.assertIn(key, prev_all_objs.keys())
 
-        with open('file.json', mode='r', encoding='utf-8') as file:
-            dict_loaded = json.load(file)
+        if os.path.exists(self.file_path):
+            with open('file.json', mode='r', encoding='utf-8') as file:
+                dict_loaded = json.load(file)
 
-            self.assertIs(type(dict_loaded), dict)
-            self.assertNotIn(key, dict_loaded.keys())
+                self.assertIs(type(dict_loaded), dict)
+                self.assertNotIn(key, dict_loaded.keys())
 
         storage.save()
 
