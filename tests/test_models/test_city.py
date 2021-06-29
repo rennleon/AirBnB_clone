9104db62977e6fffe40a7aeea19326d2c946642f
AlexisCORAN <alexis_ca_@outlook.com>
2021-06-29 17:36:59 +0000

    Test cases class for Amenity, City and Review

--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -7,5 +7,16 @@ from models.city import City
 
 
 class TestCity(unittest.TestCase):
-    """" Test cases class """
-    pass
+    """" Test cases class for City"""
+
+    def test_state_id(self):
+        """Test for place_id attribute"""
+        city = City()
+        self.assertTrue(hasattr(city, "state_id"))
+        self.assertEqual(city.state_id, "")
+
+    def test_name(self):
+        """Test for name attribute"""
+        city = City()
+        self.assertTrue(hasattr(city, "name"))
+        self.assertEqual(city.name, "")
