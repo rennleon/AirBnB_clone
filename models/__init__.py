d56f3309dcc748e841f508ae5b89945944669ef0
AlexisCORAN <alexis_ca_@outlook.com>
2021-06-29 12:45:09 +0000

    Updated first line in scripts

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,8 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 """ This module handles initialization """
 
 from models.engine.file_storage import FileStorage
+
 
 storage = FileStorage()
 storage.reload()
