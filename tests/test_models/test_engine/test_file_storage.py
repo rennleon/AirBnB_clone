597fc64eb3dc5573e17517f173bb1be5e08cac4c
renatoleon <2829@holbertonschool.com>
2021-06-29 19:18:56 +0000

    Add try catch block

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -132,12 +132,14 @@ class TestFileStorage(unittest.TestCase):
 
         self.assertIn(key, prev_all_objs.keys())
 
-        if os.path.exists('file.json'):
+        try:
             with open('file.json', mode='r', encoding='utf-8') as file:
                 dict_loaded = json.load(file)
 
                 self.assertIs(type(dict_loaded), dict)
                 self.assertNotIn(key, dict_loaded.keys())
+        except:
+            pass
 
         storage.save()
 
