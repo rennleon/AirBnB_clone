a6cea17ba16a0f1e30e407327ed9a408a0a94e33
renatoleon <2829@holbertonschool.com>
2021-06-30 22:04:47 +0000

    Update BaseModel tests

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,228 +1,134 @@
 #!/usr/bin/python3
-"""
-    This module contains test cases for base_case.py
-"""
-import unittest
-import pep8
-from uuid import uuid4
-from time import sleep
+"""Test BaseModel- Comproving expectect outputs and documentation"""
 from datetime import datetime
-from models.base_model import BaseModel
+import time
+import unittest
+# from pep8 import pycodestyle
+import models
+import inspect
+from unittest import mock
+
+BaseModel = models.base_model.BaseModel
+mod_doc = models.base_model.__doc__
+
+
+class TestDocs(unittest.TestCase):
+    """Test documentation and style"""
+    @classmethod
+    def setUpClass(self):
+        """Setup for dosctring"""
+        self.base_f = inspect.getmembers(BaseModel, inspect.isfunction)
+
+    # def testing_pep8(self):
+    # """testing that BaseModel.py passes pep8"""
+    # for path in ['models/base_model.py',
+    # "tests/test_models/test_base_model.py"]:
+    # with self.suptest(path=path):
+    # err = pycodestyle.Checker(path).check_all()
+    # self.assertEqual(err, 0)
+
+    def test_module_docstring(self):
+        """Test for the existence of module docstring"""
+        self.assertIsNot(mod_doc, None,
+                         "base_model.py needs a docstring")
+        self.assertTrue(len(mod_doc) > 1,
+                        "base_model.py needs a docstring")
+
+    def test_dosctring(self):
+        """Testing documentation"""
+        self.assertIsNot(mod_doc, None,
+                         "base_model.py needs a doctring")
+        self.assertTrue(len(mod_doc) > 1,
+                        "base_model.py needs a docstring")
 
 
 class TestBaseModel(unittest.TestCase):
-    """" Test cases class for Base Model """
-    pass
+    """testing BaseModel Class"""
+    @mock.patch('models.storage')
+    def test_instances(self, mock_storage):
+        """Testing that object is correctly created"""
+        instance = BaseModel()
+        self.assertIs(type(instance), BaseModel)
+        instance.name = "Holberton"
+        instance.number = 89
+        expectec_attrs_types = {
+            "id": str,
+            "created_at": datetime,
+            "updated_at": datetime,
+            "name": str,
+            "number": int
+        }
+        # testing types and attr names
+        for attr, types in expectec_attrs_types.items():
+            with self.subTest(attr=attr, typ=types):
+                self.assertIn(attr, instance.__dict__)
+                self.assertIs(type(instance.__dict__[attr]), types)
+        self.assertTrue(mock_storage.new.called)
+        self.assertEqual(instance.name, "Holberton")
+        self.assertEqual(instance.number, 89)
 
-    # def setUp(self) -> None:
-    #     """ Setup function to append the objects to the test """
-    #     super().setUp()
-    #     self.obj = BaseModel()
-    #     self.obj2 = BaseModel()
+    def test_datetime(self):
+        """testing correct datetime assignation
+        correct assignation of created_at and updated_at"""
+        created_at = datetime.now()
+        instance1 = BaseModel()
+        updated_at = datetime.now()
+        self.assertEqual(created_at <= instance1.created_at
+                         <= updated_at, True)
+        time.sleep(1)
+        created_at = datetime.now()
+        instance2 = BaseModel()
+        updated_at = datetime.now()
+        self.assertTrue(created_at <= instance2.created_at <= updated_at, True)
+        self.assertEqual(instance1.created_at, instance1.created_at)
+        self.assertEqual(instance2.updated_at, instance2.updated_at)
+        self.assertNotEqual(instance1.created_at, instance2.created_at)
+        self.assertNotEqual(instance1.updated_at, instance2.updated_at)
 
-    # def test_pep8_base_model(self):
-    #     """pep8 test.
-    #     Makes sure the Python code is up to the pep8 standard.
-    #     """
-    #     syntax = pep8.StyleGuide(quit=True)
-    #     check = syntax.check_files(['models/base_model.py'])
-    #     self.assertEqual(
-    #         check.total_errors, 0,
-    #         "Found code style errors (and warnings)."
-    #     )
+    def test_uuid(self):
+        """testing uuid"""
+        instance1 = BaseModel()
+        instance2 = BaseModel()
+        for instance in [instance1, instance2]:
+            tuuid = instance.id
+            with self.subTest(uuid=tuuid):
+                self.assertIs(type(tuuid), str)
 
-    # def test_unique_id(self):
-    #     """ Test the creation of unique id's for each instance """
-    #     obj1 = BaseModel()
-    #     sleep(0.01)
-    #     obj2 = BaseModel()
+        self.assertNotEqual(instance1.id, instance2.id)
 
-    #     self.assertNotEqual(obj1.id, obj2.id)
+    def test_dictionary(self):
+        """testing to_dict correct funtionality"""
+        instance3 = BaseModel()
+        instance3.name = "Holbies"
+        instance3.my_number = 89
+        new_inst = instance3.to_dict()
+        expectec_attrs = ["id",
+                          "created_at",
+                          "updated_at",
+                          "name",
+                          "my_number",
+                          "__class__"]
+        self.assertCountEqual(new_inst.keys(), expectec_attrs)
+        self.assertEqual(new_inst['__class__'], 'BaseModel')
+        self.assertEqual(new_inst['name'], 'Holbies')
+        self.assertEqual(new_inst['my_number'], 89)
 
-    # def test_attr_types(self):
-    #     """ Test the types of the default attrs of a BaseModel instance """
-    #     self.assertIs(type(self.obj.id), str)
-    #     self.assertIs(type(self.obj.created_at), datetime)
-    #     self.assertIs(type(self.obj.updated_at), datetime)
+    def test_str_method(self):
+        """testing str method, checking output"""
+        instance4 = BaseModel()
+        strr = "[BaseModel] ({}) {}".format(instance4.id, instance4.__dict__)
+        self.assertEqual(strr, str(instance4))
 
-    # def test_time_attrs(self):
-    #     """ Test for BaseModel time fields """
-    #     self.assertEqual(self.obj.created_at, self.obj.updated_at)
-    #     self.assertLess(self.obj.created_at, self.obj2.created_at)
-
-    # def test__str__method(self):
-    #     """ Test __str__ method """
-    #     str_rep = "[{}] ({}) {}"\
-    #         .format(self.obj.__class__.__name__,
-    #                 self.obj.id, self.obj.__dict__)
-    #     self.assertEqual(self.obj.__str__(), str_rep)
-
-    # def test_to_dict_method(self):
-    #     """ Test to_dict method """
-    #     from_dict = self.obj.__dict__
-    #     from_to_dict = self.obj.to_dict()
-
-    #     self.assertIs(type(from_to_dict), dict)
-    #     self.assertEqual(BaseModel.__name__, from_to_dict['__class__'])
-    #     self.assertEqual(from_dict['id'], from_to_dict['id'])
-
-    # def test_to_dict_method_with_one_param(self):
-    #     """ test to_dict method with one param """
-    #     regex = 'takes 1 positional argument but 2 were given'
-    #     with self.assertRaisesRegex(TypeError, regex):
-    #         self.obj.to_dict(None)
-    #     with self.assertRaisesRegex(TypeError, regex):
-    #         self.obj.to_dict([])
-    #     with self.assertRaisesRegex(TypeError, regex):
-    #         self.obj.to_dict([1, 2, 3])
-    #     with self.assertRaisesRegex(TypeError, regex):
-    #         self.obj.to_dict({})
-    #     with self.assertRaisesRegex(TypeError, regex):
-    #         self.obj.to_dict({1, 2, 3})
-    #     with self.assertRaisesRegex(TypeError, regex):
-    #         self.obj.to_dict(True)
-    #     with self.assertRaisesRegex(TypeError, regex):
-    #         self.obj.to_dict(False)
-    #     with self.assertRaisesRegex(TypeError, regex):
-    #         self.obj.to_dict(dict())
-    #     with self.assertRaisesRegex(TypeError, regex):
-    #         self.obj.to_dict({'id': 123, 'created_at': datetime.now()})
-    #     with self.assertRaisesRegex(TypeError, regex):
-    #         self.obj.to_dict('Ranmod value')
-
-    # def test_save_method(self):
-    #     """ Test save method """
-    #     objid = self.obj.id
-    #     self.assertEqual(self.obj.id, objid)
-    #     self.assertEqual(self.obj.created_at, self.obj.updated_at)
-
-    #     sleep(0.01)
-    #     self.obj.save()
-
-    #     self.assertEqual(self.obj.id, objid)
-    #     self.assertNotEqual(self.obj.created_at, self.obj.updated_at)
-    #     self.assertLess(self.obj.created_at, self.obj.updated_at)
-
-    # def test_save_method_with_one_param(self):
-    #     """ Test save method by passing one param """
-    #     regex = 'takes 1 positional argument but 2 were given'
-    #     with self.assertRaisesRegex(TypeError, regex):
-    #         self.obj.save(None)
-    #     with self.assertRaisesRegex(TypeError, regex):
-    #         self.obj.save([])
-    #     with self.assertRaisesRegex(TypeError, regex):
-    #         self.obj.save([1, 2, 3])
-    #     with self.assertRaisesRegex(TypeError, regex):
-    #         self.obj.save({})
-    #     with self.assertRaisesRegex(TypeError, regex):
-    #         self.obj.save({1, 2, 3})
-    #     with self.assertRaisesRegex(TypeError, regex):
-    #         self.obj.save(True)
-    #     with self.assertRaisesRegex(TypeError, regex):
-    #         self.obj.save(False)
-    #     with self.assertRaisesRegex(TypeError, regex):
-    #         self.obj.save(dict())
-    #     with self.assertRaisesRegex(TypeError, regex):
-    #         self.obj.save({'id': 123, 'created_at': datetime.now()})
-    #     with self.assertRaisesRegex(TypeError, regex):
-    #         self.obj.save('Ranmod value')
-
-    # def test_create_BaseModel_from_dictionary_as_kwargs(self):
-    #     """ Test for BaseModel instance creation using kwargs """
-    #     id = str(uuid4())
-    #     now = datetime.now().isoformat()
-    #     my_dict = {
-    #         'id': id,
-    #         'name': 'Jhon Smith',
-    #         'created_at': now,
-    #         'updated_at': now
-    #     }
-    #     obj1 = BaseModel(**my_dict)
-
-    #     self.assertEqual(obj1.id, id)
-    #     self.assertEqual(obj1.name, 'Jhon Smith')
-    #     self.assertEqual(now, obj1.created_at.isoformat())
-    #     self.assertEqual(now, obj1.updated_at.isoformat())
-
-    # def test_create_BaseModel_from_dictionary_as_kwargs_valid_attrs(self):
-    #     """ Test cases for instantiation from kwargs """
-    #     id = str(uuid4())
-    #     now = datetime.now().isoformat()
-    #     my_dict = {
-    #         'id': id,
-    #         '__class__': 'dict',
-    #         'created_at': now,
-    #         'updated_at': now
-    #     }
-    #     obj1 = BaseModel(**my_dict)
-
-    #     self.assertEqual(obj1.id, id)
-    #     self.assertIs(obj1.__class__, BaseModel)
-    #     self.assertEqual(now, obj1.created_at.isoformat())
-    #     self.assertEqual(now, obj1.updated_at.isoformat())
-
-    # def test_create_BaseModel_from_dictionary_as_kwargs_empty(self):
-    #     """ Test for BaseModel instance creation using empty kwargs """
-    #     empty_dict = dict()
-    #     obj1 = BaseModel(**empty_dict)
-
-    #     self.assertIs(type(obj1), BaseModel)
-    #     self.assertIs(type(obj1.id), str)
-    #     self.assertIs(type(obj1.created_at), datetime)
-    #     self.assertIs(type(obj1.updated_at), datetime)
-
-    # def test_create_BaseModel_from_dictionary_as_kwargs_diff_fields(self):
-    #     """ Test for BaseModel instance creation using kwargs """
-    #     my_dict = {
-    #         'name': 'Jhon Smith',
-    #         'age': 34
-    #     }
-
-    #     obj = BaseModel(**my_dict)
-
-    #     self.assertEqual(obj.name, 'Jhon Smith')
-    #     self.assertEqual(obj.age, 34)
-
-    #     regex = "object has no attribute 'id'"
-    #     with self.assertRaisesRegex(AttributeError, regex):
-    #         obj.id
-
-    #     regex = "object has no attribute 'created_at'"
-    #     with self.assertRaisesRegex(AttributeError, regex):
-    #         obj.created_at
-
-    #     regex = "object has no attribute 'updated_at'"
-    #     with self.assertRaisesRegex(AttributeError, regex):
-    #         obj.updated_at
-
-    # def test_create_BaseModel_from_dictionary_as_kwargs_wrong_format(self):
-    #     """ Test for BaseModel instance creation wrong kwargs types """
-    #     id = uuid4()
-    #     now = datetime.now()
-    #     my_dict = {
-    #         'id': id,
-    #         'name': 'Jhon Smith',
-    #         'created_at': now,
-    #         'updated_at': now
-    #     }
-
-    #     regex = 'must be str, not datetime.datetime'
-    #     with self.assertRaisesRegex(TypeError, regex):
-    #         BaseModel(**my_dict)
-
-    # def test_create_BaseModel_from_args(self):
-    #     """ Test for BaseModel instance creation using args """
-    #     args = [True, 'Random', 34.2]
-    #     obj = BaseModel(*args)
-
-    #     self.assertIs(type(obj.id), str)
-    #     self.assertIs(type(obj.created_at), datetime)
-    #     self.assertIs(type(obj.updated_at), datetime)
-
-    # def test_create_new_attrs(self):
-    #     """ Test for BaseModel attrs creation """
-    #     self.obj.name = 'Jhon'
-    #     self.obj.age = 46
-
-    #     self.assertEqual(self.obj.name, 'Jhon')
-    #     self.assertEqual(self.obj.age, 46)
+    @mock.patch('models.storage')
+    def test_save_method(self, mock_storage):
+        """test save method and if it updates
+        "updated_at" calling storage.save"""
+        instance4 = BaseModel()
+        created_at = instance4.created_at
+        updated_at = instance4.updated_at
+        instance4.save()
+        new_created_at = instance4.created_at
+        new_updated_at = instance4.updated_at
+        self.assertNotEqual(updated_at, new_updated_at)
+        self.assertEqual(created_at, new_created_at)
+        self.assertTrue(mock_storage.save.called)
