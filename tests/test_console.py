dd5fe5a2d0e8055fa116c3ea68dae590d0997cb8
AlexisCORAN <alexis_ca_@outlook.com>
2021-06-30 21:22:10 +0000

    Updated tes_console with test for command help

--- tests/test_console.py
+++ tests/test_console.py
@@ -20,14 +20,36 @@ class test_console(unittest.TestCase):
     def test_command_empty_line(self):
         """Test for the empty line"""
         with patch('sys.stdout', new=StringIO()) as f:
-            self.assertEqual("", f.getvalue().strip())
+            self.assertEqual("", f.getvalue())
 
     def test_commnad_quit(self):
         """Test for command quit in the console"""
-        with patch('sys.stdout', new=StringIO()) as f:
-            self.assertTrue(HBNBCommand().onecmd('quit'))
+        self.assertTrue(HBNBCommand().onecmd('quit'))
 
     def test_command_EOF(self):
         """Test for command EOF in the console"""
-        with patch('sys.stdout', new=StringIO()) as f:
-            self.assertTrue(HBNBCommand().onecmd('EOF'))
+        self.assertTrue(HBNBCommand().onecmd('EOF'))
+
+    def test_command_help_EOF(self):
+        """Test for command help 'EOF' in the console"""
+        com_h = 'Quit command to exit the program'
+        with patch('sys.stdout', new=StringIO()) as output:
+            self.assertFalse(HBNBCommand().onecmd('help EOF'))
+            self.assertEqual(com_h, output.getvalue().strip())
+
+    def test_command_help_quit(self):
+        """Test for command help 'quit' in the console"""
+        com_h = 'Quit command to exit the program'
+        with patch('sys.stdout', new=StringIO()) as output:
+            self.assertFalse(HBNBCommand().onecmd('help quit'))
+            self.assertEqual(com_h, output.getvalue().strip())
+
+    def test_command_help(self):
+        """Test for command help in the console"""
+        com_h = ('Documented commands (type help <topic>):' +
+                 '\n========================================' +
+                 '\nEOF  all  count  create  destroy  help' +
+                 '  quit  show  update')
+        with patch('sys.stdout', new=StringIO()) as output:
+            self.assertFalse(HBNBCommand().onecmd('help'))
+            self.assertEqual(com_h, output.getvalue().strip())
