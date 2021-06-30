c062c4f765a77fab68d2e9f52dcb46bd11351c56
renatoleon <2829@holbertonschool.com>
2021-06-30 22:38:25 +0000

    Update tests for BaseModel

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -13,28 +13,6 @@ mod_doc = models.base_model.__doc__
 
 class TestBaseModel(unittest.TestCase):
     """testing BaseModel Class"""
-    # def test_dictionary(self):
-    #     """testing to_dict correct funtionality"""
-    #     instance3 = BaseModel()
-    #     instance3.name = "Holbies"
-    #     instance3.my_number = 89
-    #     new_inst = instance3.to_dict()
-    #     expectec_attrs = ["id",
-    #                       "created_at",
-    #                       "updated_at",
-    #                       "name",
-    #                       "my_number",
-    #                       "__class__"]
-    #     self.assertCountEqual(new_inst.keys(), expectec_attrs)
-    #     self.assertEqual(new_inst['__class__'], 'BaseModel')
-    #     self.assertEqual(new_inst['name'], 'Holbies')
-    #     self.assertEqual(new_inst['my_number'], 89)
-
-    def test_str_method(self):
-        """testing str method, checking output"""
-        instance4 = BaseModel()
-        strr = "[BaseModel] ({}) {}".format(instance4.id, instance4.__dict__)
-        self.assertEqual(strr, str(instance4))
 
     @mock.patch('models.storage')
     def test_save_method(self, mock_storage):
