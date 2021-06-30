7c406bbe94e4df92dc4b3f22ca37447406c49c1e
renatoleon <2829@holbertonschool.com>
2021-06-30 22:31:02 +0000

    Update tests for BaseModel

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -12,35 +12,12 @@ BaseModel = models.base_model.BaseModel
 mod_doc = models.base_model.__doc__
 
 
-class TestDocs(unittest.TestCase):
-    """Test documentation and style"""
-    @classmethod
-    def setUpClass(self):
-        """Setup for dosctring"""
-        self.base_f = inspect.getmembers(BaseModel, inspect.isfunction)
-
-    # def testing_pep8(self):
-    # """testing that BaseModel.py passes pep8"""
-    # for path in ['models/base_model.py',
-    # "tests/test_models/test_base_model.py"]:
-    # with self.suptest(path=path):
-    # err = pycodestyle.Checker(path).check_all()
-    # self.assertEqual(err, 0)
-
-    # def test_module_docstring(self):
-    #     """Test for the existence of module docstring"""
-    #     self.assertIsNot(mod_doc, None,
-    #                      "base_model.py needs a docstring")
-    #     self.assertTrue(len(mod_doc) > 1,
-    #                     "base_model.py needs a docstring")
-
-    # def test_dosctring(self):
-    #     """Testing documentation"""
-    #     self.assertIsNot(mod_doc, None,
-    #                      "base_model.py needs a doctring")
-    #     self.assertTrue(len(mod_doc) > 1,
-    #                     "base_model.py needs a docstring")
-
+# class TestDocs(unittest.TestCase):
+#     """Test documentation and style"""
+#     @classmethod
+#     def setUpClass(self):
+#         """Setup for dosctring"""
+#         self.base_f = inspect.getmembers(BaseModel, inspect.isfunction)
 
 class TestBaseModel(unittest.TestCase):
     """testing BaseModel Class"""
